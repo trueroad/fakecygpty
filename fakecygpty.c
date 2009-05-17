31e1d5d424dc78c581708984d244c09ce3ecccbd
horiguti <horiguti>
2009-05-17 22:10:15 +0900

    Hide console window raised on Windows 7 RC

--- fakecygpty.c
+++ fakecygpty.c
@@ -15,8 +15,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with fiber, see the file COPYING.  If not, write to the Free
- * Software Foundation Inc., 59 Temple Place - Suite 330, Boston,
+ * along with fakecygpty, see the file COPYING.  If not, write to the
+ * Free Software Foundation Inc., 59 Temple Place - Suite 330, Boston,
  * MA 02111-1307, USA.
  */
 
@@ -26,6 +26,7 @@
  *
  *    09 Jun, 2005 : Version 1.0.0 - first release.
  *    15 Jun, 2005 : Version 1.0.1 - bug fix and change coding style.
+ *    15 May, 2009 : Version 1.0.2 - Work around for Windows 7 RC
  */
 
 /*
@@ -49,6 +50,25 @@
 /* global variables */
 int child_pid;		/* pid of child proces  */
 int masterfd;		/* fd of pty served to child process */
+
+
+/* Console window of child process is made visible on Windows 7 RC, 
+   Forcefully make it as workaround... */
+BOOL CALLBACK
+enum_windows_proc (HWND hWnd, LPARAM pid) {
+  DWORD procid;
+  GetWindowThreadProcessId (hWnd, &procid);
+  
+  if (pid == procid)
+    ShowWindow (hWnd, SW_HIDE);
+}
+
+void
+hide_console_window (void)
+{
+  EnumWindows (enum_windows_proc, GetCurrentProcessId ());
+}
+
 
 /* Create pty and fork/exec target process */
 /* This function sets child_pid and masterfd */
@@ -78,7 +98,11 @@ exec_target(char* argv[])
 
       setsid();
 
+      /* this open raises console window on Windows 7 RC,
+	 hide_console_window () hides it. */
       slave = open (ptsname (masterfd), O_RDWR);
+      hide_console_window ();
+
       if (slave < 0)
 	{
 	  perror ("Failed to open slave fd");
